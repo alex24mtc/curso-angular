1de29ea6f53a7b57cac6e7354fde1825c3f4205c
alex24mtc <alex25mtc@gmail.com>
2021-03-04 10:36:41 +0100

    instalados modulos de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //es como un div 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  nombre= 'alex marí';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Component1Component } from './components/component1/component1.component';
+
+// 2. Add your credentials from step 1
+const config = {
+  apiKey: "AIzaSyBstVZunDSDMtfZ_capJJ_xOi82uAT7wqQ",
+  authDomain: "angular-7a917.firebaseapp.com",
+  projectId: "angular-7a917",
+  storageBucket: "angular-7a917.appspot.com",
+  messagingSenderId: "409498304481",
+  appId: "1:409498304481:web:72bf6995d72c19d531dca0",
+  measurementId: "G-X4PB5GBSR3"
+
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
