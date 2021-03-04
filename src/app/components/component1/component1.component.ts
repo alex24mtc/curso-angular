ea561797ef93e9ce056f53e546b6c7b5c00885a6
alex24mtc <alex25mtc@gmail.com>
2021-03-04 13:01:38 +0100

    listamos nombres de personas

--- src/app/components/component1/component1.component.ts
+++ src/app/components/component1/component1.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Persona{
+  nombre:string
+  apellido:string
+  edad?:number
+}
 @Component({
   selector: 'app-component1',
   templateUrl: './component1.component.html',
   styleUrls: ['./component1.component.css']
 })
-export class Component1Component implements OnInit {
 
-  constructor() { }
+
+export class Component1Component implements OnInit {
+  
+  // null, undefined, 0, '', FALSE
+  
+  alumnos:Persona[] = [
+    {
+      nombre:'Marc',
+      apellido:'Estalella',
+      edad:26
+    },
+    {
+      nombre:'Zdravko',
+      apellido:'Marinov',
+      edad:21
+    },
+    {
+      nombre:'Julia',
+      apellido:'Abad'
+    },
+  ]
+
+  constructor() { 
+    console.log('implementamos el constructor');
+  }
 
   ngOnInit(): void {
+    console.log('NOMBRES',this.alumnos)
   }
 
 }
